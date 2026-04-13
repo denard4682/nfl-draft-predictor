cf0d6e5b43e26ce533839306b976462aac6e4179
denard4682 <corey4682@gmail.com>
2026-04-12 21:57:50 -0400

    fix missing load_model function

---
+++ src/predict.py
@@ -0,0 +1,22 @@
+import joblib
+import numpy as np
+
+'''def load_model(clf_path, reg_path):
+    clf = joblib.load(clf_path)
+    reg = joblib.load(reg_path)
+    return clf, reg
+
+
+def predict_player(clf, reg, features):
+    features = np.array(features).reshape(1, -1)
+    drafted = clf.predict(features)[0]
+
+    if drafted == 1:
+        pick = reg.predict(features)[0]
+        return drafted, pick
+    
+    return drafted, None'''
+
+def load_model(path):
+    return joblib.load(path)
+    
